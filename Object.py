0f23681cbc183ba162eae4284fce559c68d5e650
patrykz <patryk.zajkowski@infotech.edu.pl>
2022-02-14 09:23:50 +0100

    klasa game i klasa object

--- Object.py
+++ Object.py
@@ -1,8 +1,12 @@
+import pygame
+
 from main import Game
 
 class Object:
     def __init__(self,x,y):
-        self.x = x
-        self.y = y
+        self.pos_x = x
+        self.pos_y = y
+        self.rect = pygame.Rect(x,y)
         Game.Objects.append(self)
     def render(self):
+        pass
