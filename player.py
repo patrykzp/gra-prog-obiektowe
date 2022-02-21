f0fea9180f371b1c8be0b9e1d2cc83e24e4f7edb
Patryk <patryk.mantaj@infotech.edu.pl>
2022-02-21 09:46:15 +0100

    player moves

--- player.py
+++ player.py
@@ -9,5 +9,10 @@ class Character(Object):
         self.size = size
 
 class Player(Character):
-    def __init__(self,pos_x, pos_y, facing, HP, size,game):
-        super().__init__(pos_x, pos_y, facing, HP, size,game)
+    def __init__(self,pos_x, pos_y, facing, HP, size, game):
+        super().__init__(pos_x, pos_y, facing, HP, size, game)
+
+    def update(self):
+        self.pos_x += self.game.input.MoveDirection[0]
+        self.pos_y += self.game.input.MoveDirection[1]
+        super().update()
