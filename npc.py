d4925896f08a5408926ff9e87b2ed45c4ff6bd50
patrykz <patryk.zajkowski@infotech.edu.pl>
2022-02-21 08:35:44 +0100

    nowe

--- npc.py
+++ npc.py
@@ -5,10 +5,8 @@ import player
 class NPC(player.Character):
    def __init__(self,pos_x, pos_y, facing, HP, size):
        super().__init__(pos_x, pos_y, facing, HP, size)
-       pygame.sprite.Sprite.__init__(self)
        pos_x = random.randint(50, 650)
        pos_y = random.randint(50, 450)
-       self.rect = pygame.Rect(self.pos_x, self.pos_y, self.size, self.facing)
 
    def state(self):
        pass
--- player.py
+++ player.py
@@ -1,17 +1,12 @@
 import pygame
+import main
 
-
-class Character:
+class Character(main.Object):
     def __init__(self, pos_x, pos_y, facing, HP, size):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+        super().__init__(pos_x,pos_y,size)
         self.facing = facing
         self.HP = HP
         self.size = size
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.image.load("Player.png")
-        self.rect = pygame.Rect(self.pos_x, self.pos_y, self.size, self.facing)
 
 class Player(Character):
     def __init__(self,pos_x, pos_y, facing, HP, size):
